01cfed7dcc609851e39f3e6a8c467248c0ed6ee6
Sohn Hyosun <48785364+0hyodorie0@users.noreply.github.com>
2022-02-08 16:29:23 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $(function() {
     // 미니홈피 메뉴 선택 색상변경
+    $('.minihome_menu li').first().find('a').addClass('active');
     $('.minihome_menu a').on('click', function(e) {
-        e.preventDefault();
-        $(this).parents('li').css('border', '1px solid red');
-        $(this).addClass('active');
+
+        if ($(this).hasClass('active')) {
+            $(this).parents().find('a').removeClass('active')
+        } else {
+            $(this).parents().find('a').removeClass('active')
+            $(this).addClass('active');
+        }
     });
 });
